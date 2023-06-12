8d5673a05d0db65c0e896fe897ff41089e622ec2
95dewadew <41128556+95dewadew@users.noreply.github.com>
2023-06-12 18:30:38 +0700

    Update newagent.sh

--- newagent.sh
+++ newagent.sh
@@ -1629,15 +1629,14 @@ installXrayService() {
 		cat <<EOF >/etc/systemd/system/xray.service
 [Unit]
 Description=Xray Service
-Documentation=https://github.com/XTLS/Xray-core
+Documentation=https://github.com/xtls
 After=network.target nss-lookup.target
-Wants=network-online.target
 
 [Service]
-Type=simple
 User=root
-CapabilityBoundingSet=CAP_NET_BIND_SERVICE CAP_NET_RAW
-NoNewPrivileges=yes
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
 ExecStart=${execStart}
 Restart=on-failure
 RestartPreventExitStatus=23
